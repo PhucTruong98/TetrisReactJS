6f46631f44d590de71f035c9d03aa93d2c65dee2
PhucTruong98 <31789447+PhucTruong98@users.noreply.github.com>
2022-02-16 16:49:55 -0800

    added connection for control and grid board and next block

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import GridSquare from './components/GridSquare';
 import MessagePopup from './components/MessagePopup';
 import NextBlock from './components/NextBlock';
 import ScoreBoard from './components/ScoreBoard';
+import reducers from './reducers'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+
+const store = createStore(reducers)
+
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Tetris Redux</h1>
@@ -20,6 +28,8 @@ function App() {
      
 
     </div>
+
+    </Provider>
   );
 
 }
